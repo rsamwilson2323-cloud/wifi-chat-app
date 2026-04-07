3ec83998099c6405fc0d58b243f5f969818aa3ae
Sam Wilson <rsamwilson2323@gmail.com>
2026-04-07 06:55:54 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,8 +6,6 @@ echo Starting WiFi Chat App Server
 echo ==============================
 echo.
 
-cd /d "D:\main coding\wifi-chat-app\wifi-chat-app"
-
 echo Installing required libraries...
 npm install
 
